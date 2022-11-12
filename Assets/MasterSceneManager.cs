0b4b3b3973b1fe7e4cdb312437ff65f3869ed290
Schrotti <florian-ostermeier@freenet.de>
2022-11-12 23:45:02 +0100

    completed master sound; initial scene management

---
+++ Assets/MasterSceneManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MasterSceneManager : MonoBehaviour
+{
+    public static MasterSceneManager Instance;
+
+    public List<SceneLoadingStruct> gameLoopScenes;
+    public List<SceneLoadingStruct> mainMenueScenes;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        Instance = this;
+    }
+    public void LoadGame()
+    {
+        foreach(SceneLoadingStruct sceneLoadingStruct in gameLoopScenes)
+        {
+            SceneManager.LoadScene(sceneLoadingStruct.sceneName, sceneLoadingStruct.loadSceneMode);
+        }
+    }
+    public void LoadMainMenue()
+    {
+        foreach (SceneLoadingStruct sceneLoadingStruct in gameLoopScenes)
+        {
+            SceneManager.LoadScene(sceneLoadingStruct.sceneName, sceneLoadingStruct.loadSceneMode);
+        }
+    }
+    public void ExitGame()
+    {
+        throw new NotImplementedException();
+    }
+}
+[System.Serializable]
+public struct SceneLoadingStruct
+{
+    public string sceneName;
+    public LoadSceneMode loadSceneMode; 
+}
