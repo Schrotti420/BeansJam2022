13262f92145bae8fcbb93464d9bcadcc687e29ae
Louis <79322212+LouisMichelon@users.noreply.github.com>
2022-11-12 22:39:48 +0100

    MainMenu , SettingsMenu

--- Assets/Scripts/MainMenu/ButtonClickEvents.cs
+++ Assets/Scripts/MainMenu/ButtonClickEvents.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class ButtonClickEvents: MonoBehaviour
 {
+    public GameObject startMenu;
+    public GameObject settingsMenu;
+    //quit application
     public void doExitGame()
     {
         Application.Quit();
+    }
+
+    //go to settings page
+    public void settingsClick()
+    {
+        startMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    //go from settings page back to main menu page
+    public void backClick()
+    {
+        settingsMenu.SetActive(false);
+        startMenu.SetActive(true);
+    }
+
+    //adjust the ingame volume
+    public void volumeAdjust()
+    {
+
     }
 }
