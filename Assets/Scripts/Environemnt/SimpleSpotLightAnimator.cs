47582d6ca546ea7452f7af9396a5a887648c4899
Schrotti <florian-ostermeier@freenet.de>
2022-11-13 19:20:15 +0100

    updated lighting

--- Assets/Scripts/Environemnt/SimpleSpotLightAnimator.cs
+++ Assets/Scripts/Environemnt/SimpleSpotLightAnimator.cs
@@ -15,16 +15,18 @@ public class SimpleSpotLightAnimator : MonoBehaviour
 
     private void Start()
     {
-        StartSpotlightAnimation();
+        StartCoroutine(StartSpotlightAnimation());
     }
-    private void StartSpotlightAnimation()
+    private IEnumerator StartSpotlightAnimation()
     {
+        yield return new WaitForSecondsRealtime(Random.value * 2);
+
         Sequence zse = DOTween.Sequence();
         Sequence yse = DOTween.Sequence();
-        zse.Append(zRotationRoot.DOLocalRotate(new Vector3(0f,90f, 0f), zDuration + Random.value, RotateMode.Fast));
-        zse.Append(zRotationRoot.DOLocalRotate(new Vector3(0f, 90f, 360f), zDuration + Random.value, RotateMode.Fast));
-        yse.Append(yRotationRoot.DOLocalRotate(new Vector3(0f, 70, 0f), yDuration + Random.value, RotateMode.LocalAxisAdd));
-        yse.Append(yRotationRoot.DOLocalRotate(new Vector3(0f, 0f, 0f), yDuration + Random.value, RotateMode.Fast));
+        zse.Append(zRotationRoot.DOLocalRotate(new Vector3(0f,90f, 50f), zDuration + Random.value, RotateMode.Fast).SetEase(Ease.Linear));
+        zse.Append(zRotationRoot.DOLocalRotate(new Vector3(0f, 90f, 150f), zDuration + Random.value, RotateMode.Fast).SetEase(Ease.Linear));
+        //yse.Append(yRotationRoot.DOLocalRotate(new Vector3(0f, 70, 0f), yDuration + Random.value, RotateMode.LocalAxisAdd));
+        //yse.Append(yRotationRoot.DOLocalRotate(new Vector3(0f, 0f, 0f), yDuration + Random.value, RotateMode.Fast));
 
         zse.SetLoops(-1, LoopType.Yoyo);
         yse.SetLoops(-1, LoopType.Yoyo);
