08c81856a6bc878d999e21af7a9dcf69fdf10656
Louis <79322212+LouisMichelon@users.noreply.github.com>
2022-11-13 13:07:14 +0100

    boxColliderBar

---
+++ Assets/boxColliderEvent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxColliderEvent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //if you collide with the Bar box collider then open the bar menu
+    private void OnTriggerEnter(Collision other)
+    {
+        //if(other.GameObject.tag = "Bar")
+        //{
+        //
+        //}
+        
+    }
+}
